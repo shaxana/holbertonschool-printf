6aaf77fcab739e6882a8f1e82d3f9013b83d823b
Ogtay-Muradov <ogtay.muradov@gmail.com>
2024-03-30 11:04:23 +0000

    main codes fixed

--- main.h
+++ main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 #include <stdlib.h>
 #include <stdarg.h>
-
+int _putchar(char c);
+int print_char(unsigned int r, char c);
+int print_string(unsigned int r, char *s);
 int _printf(const char *format, ...);
-
+int print_decimal(unsigned int r, int s);
 #endif
