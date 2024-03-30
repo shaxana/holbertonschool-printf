b53d809948c5e1ed1e335dd7255b32e21697facc
shaxana <bd7n9d7mw@code.edu.az>
2024-03-30 11:18:28 +0000

    print_string.c added

---
+++ print_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *  * print_string - print string
+ *   * @r: leng
+ *    * @s: string
+ *     * Return: leng
+ *      */
+int print_string(unsigned int r, char *s)
+{
+		int i = 0;
+
+		if (!s)
+		{
+			_putchar('(');
+			_putchar('n');
+			_putchar('u');
+			_putchar('l');
+			_putchar('l');
+			_putchar(')');
+			r = r + 6; }
+		else
+		{
+			while (*(s + i) != '\0')
+			{
+				_putchar(*(s + i));
+				i++;
+				r++; } }
+		return (r); }
